bb4dea155d62451781e8db4feb7969cd6c6cdd45
Mika Sidwell <mika.sidwell@gmail.com>
2026-08-25 16:14:12 +0200

    fix: correct configuration structure in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     images: {
         domains: [
             "res.cloudinary.com"
         ]
+    },
+    experimental: {
+        cpus: 1
     }
 };
 
